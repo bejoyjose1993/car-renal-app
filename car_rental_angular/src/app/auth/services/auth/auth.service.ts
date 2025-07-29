1f2675301339d6d107f01f9590fc1cd0678a5d9d
Bejoy Kachappilly <bejoy@altada.com>
2025-07-29 01:48:20 +0100

    Adding BASE URL to constant

--- car_rental_angular/src/app/auth/services/auth/auth.service.ts
+++ car_rental_angular/src/app/auth/services/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.prod';
 //export const BASE_URL = ["http://localhost:8082"]
 //export const BASE_URL = ["http://16.170.223.44:8082"]
-export const BASE_URL = ["http://16.171.57.106:8082"]
+//export const BASE_URL = ["http://16.171.57.106:8082"]
+export const BASE_URL = [`${environment.apiUrl}`]
 
 @Injectable({
   providedIn: 'root'
---
+++ car_rental_angular/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'PLACEHOLDER_BASE_URL' // gets overwritten during build
+};
