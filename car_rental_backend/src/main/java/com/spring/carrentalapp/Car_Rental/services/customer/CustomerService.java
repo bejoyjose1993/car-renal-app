4a2dfb543242834d36a3108c2a28db07265bdabf
Bejoy Kachappilly <bejoy@altada.com>
2025-08-01 12:41:16 +0100

    Connecting to S3 bucket and creating a sample upload and download api

--- car_rental_backend/src/main/java/com/spring/carrentalapp/Car_Rental/services/customer/CustomerService.java
+++ car_rental_backend/src/main/java/com/spring/carrentalapp/Car_Rental/services/customer/CustomerService.java
@@ -1,7 +1,11 @@
 package com.spring.carrentalapp.Car_Rental.services.customer;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.spring.carrentalapp.Car_Rental.dto.CarDto;
 import com.spring.carrentalapp.Car_Rental.dto.RentACarDto;
 import com.spring.carrentalapp.Car_Rental.entity.Car;
@@ -16,4 +20,8 @@ public interface CustomerService {
 	List<RentACarDto> getBookingsByUserId(Long userId);
 	
 	List<Car> searchAvailableCars(String type, LocalDateTime from, LocalDateTime to);
+	
+	void uploadFile(MultipartFile file)  throws IOException;
+	
+	byte[] downloadFile(String key);
 }
