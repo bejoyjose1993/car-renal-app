a9acbc96bd40947a4de3d502d4b990597b7207b5
Bejoy Kachappilly <bejoy@altada.com>
2025-08-19 02:51:25 +0100

    Initial deploy branch with only deployment files

--- car_rental_angular/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'PLACEHOLDER_BASE_URL' // gets overwritten during build
-};
--- car_rental_mailing/src/main/java/com/car/rental/mailing/car_rental_mailing/services/NotificationConsumer.java
+++
@@ -1,6 +0,0 @@
-package com.car.rental.mailing.car_rental_mailing.services;
-
-public interface NotificationConsumer {
-
-	void consume(String message);
-}
